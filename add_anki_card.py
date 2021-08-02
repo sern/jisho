885f93bccad40496b200f235266fee1ff9d00df3
sern <martinshi2001@yahoo.com>
2021-08-02 21:41:46 +0100

    .

--- add_anki_card.py
+++ add_anki_card.py
@@ -2,10 +2,27 @@ import requests
 import pyjisho
 import lxml.html as lh
 from typing import List
+import re
 
 SERVER = "http://localhost:8765"
 DECK = "日本語語彙"
 MODEL = "japanese"
+
+
+def adverb_pattern(word: str) -> str:
+    if len(word) == 4:
+        if word[0] == word[2] and word[1] == word[3]:
+            return "ABAB"
+        if word[3] == "と":
+            if word[2] == "っ":
+                if word[2] in ["ゅ", "ゃ"]:
+                    return "Aっと"
+                else:
+                    return "ABっと"
+            else:
+                return "ABCと"
+    if len(word) == 3 and word[1] == "っ":
+        return "Aっと"
 
 
 def exists(word):
@@ -20,17 +37,22 @@ def exists(word):
     return r["error"] != None
 
 
-def add_note(word: pyjisho.SearchResultSingle, examples: str):
+def add_note(word: pyjisho.SearchResultSingle, examples: str = ""):
     if len(word.jp.kanjis) > 0:
         front = "・".join(word.jp.kanjis)
         if word.jp.hiragana:
             front += f"[{word.jp.hiragana}]"
     else:
         front = word.jp.hiragana
-    hinshi = word.hinshi
-    if len(hinshi) == 0:
+    tags = word.hinshi
+    if len(tags) == 0:
         # hinshi = input("品詞はなんですか？（名・動・形・形動・副・連語）").split(" ")
-        hinshi = ["名"]
+        tags = ["名"]
+    if "副" in tags:
+        pat = adverb_pattern(word.jp.hiragana)
+        if pat:
+            tags.append(pat)
+    print(f"Tags: {tags}")
     if exists(front):
         print("Already exists.")
         return
@@ -50,7 +72,7 @@ def add_note(word: pyjisho.SearchResultSingle, examples: str):
                         "jp-cn": word.jp_cn.definition if word.jp_cn else "",
                         "jp-en": word.jp_en.definition if word.jp_en else "",
                     },
-                    "tags": hinshi,
+                    "tags": tags,
                 }
             },
         },
@@ -62,16 +84,24 @@ def add_note(word: pyjisho.SearchResultSingle, examples: str):
         raise Exception(f"Failed to add {front}.")
 
 
+def search_and_add(query):
+    word = pyjisho.search_exact_interactive(query)
+    if word:
+        add_note(word, examples)
+    else:
+        print("Not found.")
+
+
 if __name__ == "__main__":
     import os
     import sys
 
     os.chdir(os.path.dirname(__file__))
     if len(sys.argv) == 1:
-        raise Exception(f"Please provide a query, e.g. `{sys.argv[0]} sonaeru`")
-    query = sys.argv[1]
-    examples = "\n".join(sys.argv[2:]) if len(sys.argv) > 2 else ""
-    word = pyjisho.search_exact_interactive(query)
-    if not word:
-        raise Exception("Aborted")
-    add_note(word, examples)
+        query = input("Query: ")
+    else:
+        query = sys.argv[1]
+    # examples = "\n".join(sys.argv[2:]) if len(sys.argv) > 2 else ""
+    while True:
+        search_and_add(query)
+        query = input("Query: (skip to abort): ")
--- pyjisho.py
+++ pyjisho.py
@@ -4,19 +4,8 @@ JP = 0b00000001
 JP_CN = 0b00000010
 JP_EN = 0b00000100
 
-if __name__ == "__main__":
-    import os
-    import sys
 
-    query = sys.argv[1]
-    results = search_exact_interactive(query)
-    if not results:
-        raise Exception("Aborted")
-    # search_exact(query, JP | JP_CN, JP_EN)
-    # for jp_result in results.jp:
-    #     print(jp_result.hiragana, jp_result.kanjis)
-    # flattened = [e.definition for e in results.jp + results.jp_cn + results.jp_en]
-    print(results.hinshi)
+def display_in_browser_single(results):
     flattened = [e.definition for e in [results.jp, results.jp_cn, results.jp_en]]
     HEAD = """<html lang="en">
     <head>
@@ -41,3 +30,19 @@ if __name__ == "__main__":
         webbrowser.open("file://" + out)
     else:
         print("no results found")
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+
+    query = sys.argv[1]
+    results = search_exact_interactive(query)
+    if not results:
+        raise Exception("Aborted")
+    # search_exact(query, JP | JP_CN, JP_EN)
+    # for jp_result in results.jp:
+    #     print(jp_result.hiragana, jp_result.kanjis)
+    # flattened = [e.definition for e in results.jp + results.jp_cn + results.jp_en]
+    print(results.hinshi)
+    display_in_browser_single(results)
